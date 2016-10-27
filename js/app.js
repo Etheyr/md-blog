c00ab7899b0eaaed379024178fe664e12f3cd307
axel <axel.bouillart@gmail.com>
2016-10-27 09:30:39 +0200

    connection entre les 2 serveur fait

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 	"use strict";
 
 	var app = {
-		converter :new showdown.Converter(),
-		html :null,
-		url1 :'http://192 168 1.40:1337/example.md',
-		url2 : 'http://192.168.1.40:1337/alice.md',
-		url3 : 'http://192.168.1.40:1337/menu.json',
+
+		converter : new showdown.Converter(),
+		html : null,
+		url1 :'http://localhost:2305/example.md',
+		url2 : 'http://localhost:2305/alice.md',
+		url3 : 'http://localhost:2305/menu.json',
 
 		init:function(){
 
@@ -15,12 +16,12 @@ $(document).ready(function(){
 		},
 		recupJson: function(){
 
-			$.get('http://192.168.1.40:1337/menu.json',function(data3){
+			$.get('http://localhost:2305/menu.json',function(data3){
 				var menus = data3.menu;
 				for(var i = 0; i < menus.length; i++){
-					$('#json').append('<li><a href ="http://192.168.1.40:1337'+menus[i].path+'">'+menus[i].title+'</li>');
+					$('#json').append('<li><a href ="http://localhost:2305'+menus[i].path+'">'+menus[i].title+'</li>');
 				}
-			
+
 			});
 		},
 		listeners: function(){
@@ -32,20 +33,18 @@ $(document).ready(function(){
 		},
 		recupMarkdown: function(){
 
-			$.get('http://192.168.1.40:1337/example.md',function(data1){
+			$.get('http://localhost:2305/example.md',function(data1){
 				app.html = app.converter.makeHtml(data1);
 				$('#markdown').html(app.html);
 			});
 		},	
 		recupAlice: function(){
 
-			$.get('http://192.168.1.40:1337/alice.md',function(data2){
+			$.get('http://localhost:2305/alice.md',function(data2){
 				app.html = app.converter.makeHtml(data2);
 				$('#alice').html(app.html);
 			});
 		},
-
-
 	};
 	app.init();
 });
